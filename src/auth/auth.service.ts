52bb0069aa15a8e5aac7489cd4103d233a225602
Qayyuum <qayyuum.harun@gmail.com>
2020-06-13 10:02:31 +0800

    Adding authorization (#1)
    * Added user and auth strategy
    
    * Guard task module
    
    Co-authored-by: Qayyuum Harun <qayyuum@logit.global>

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private JwtService: JwtService,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    await this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(
+      authCredentialsDto,
+    );
+
+    if (!username) {
+      throw new UnauthorizedException('Invalid credentials!');
+    }
+
+    const payload: JwtPayload = {username};
+    const accessToken = await this.JwtService.sign(payload);
+    
+    return {accessToken};
+  }
+}
