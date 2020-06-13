8e917fada611d23d0e0648ab2765305bf8e15867
Qayyuum <qayyuum.harun@gmail.com>
2020-06-13 22:31:36 +0800

    Added Logging feature (#3)
    * Added logging feature
    
    * refactor configuration
    
    Co-authored-by: Qayyuum Harun <qayyuum@logit.global>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as config from 'config'
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const serverConfig =  config.get('server');
+  await app.listen(process.env.PORT || serverConfig.port);
+  logger.log(`Application is listening on port ${process.env.PORT || serverConfig.port}`)
 }
 bootstrap();
