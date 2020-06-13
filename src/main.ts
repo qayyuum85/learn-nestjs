34ea68fd0c5b741025a74b388cf355aacb426a31
Qayyuum Harun <qayyuum@logit.global>
2020-06-13 22:01:47 +0800

    refactor configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config'
 
 async function bootstrap() {
   const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000;
-  await app.listen(port);
-  logger.log(`Application is listening on port ${port}`)
+  const serverConfig =  config.get('server');
+  await app.listen(process.env.PORT || serverConfig.port);
+  logger.log(`Application is listening on port ${process.env.PORT || serverConfig.port}`)
 }
 bootstrap();
